13c4bf11df79dbe3cbd6aacfe14a72cd03aa5660
J-Breno <joao.breno85@hotmail.com>
2024-11-17 02:28:16 -0300

    Adding React Router DOM

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -1,9 +1,8 @@
 import "./styles.css";
-import ButtonInverse from "../../components/ButtonInverse";
-import ButtonPrimary from "../../components/ButtonPrimary";
-import HeaderClient from "../../components/HeaderClient";
-import ProductDetailsCard from "../../components/ProductDetailsCard";
-import { ProductDTO } from "../../models/product";
+import ButtonInverse from "../../../components/ButtonInverse";
+import ButtonPrimary from "../../../components/ButtonPrimary";
+import ProductDetailsCard from "../../../components/ProductDetailsCard";
+import { ProductDTO } from "../../../models/product";
 
 const product: ProductDTO = {
   id: 2,
@@ -23,24 +22,21 @@ const product: ProductDTO = {
     },
     {
       id: 4,
-      name: "Importados"
-    }
+      name: "Importados",
+    },
   ],
 };
 
 export default function ProductDetails() {
   return (
-    <>
-      <HeaderClient />
       <main>
         <section id="product-details-section" className="dsc-container">
           <ProductDetailsCard product={product} />
           <div className="dsc-btn-page-container">
-            <ButtonPrimary />
-            <ButtonInverse />
+            <ButtonPrimary text="Comprar" />
+            <ButtonInverse text="Início" />
           </div>
         </section>
       </main>
-    </>
   );
 }
