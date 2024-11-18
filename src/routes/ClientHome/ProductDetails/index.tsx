8c3348e1057df651714150d203fc39ef49456337
J-Breno <joao.breno85@hotmail.com>
2024-11-18 06:33:01 -0300

    Catch request error and redirect page

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -3,13 +3,15 @@ import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
 import * as productService from "../../../services/product-service";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { ProductDTO } from "../../../models/product";
 
 export default function ProductDetails() {
   const params = useParams();
+
+  const navigate = useNavigate();
 
   const [product, setProduct] = useState<ProductDTO>();
   useEffect(() => {
@@ -19,12 +21,17 @@ export default function ProductDetails() {
       console.log(response.data);
       setProduct(response.data)
     })
+    .catch(() => {
+      navigate("/")
+    });
   }, [])
 
   return (
     <main>
       <section id="product-details-section" className="dsc-container">
-        {product && <ProductDetailsCard product={product} />}
+        {product 
+        && <ProductDetailsCard product={product} />
+        }
         <div className="dsc-btn-page-container">
           <ButtonPrimary text="Comprar" />
           <Link to={"/"}>
