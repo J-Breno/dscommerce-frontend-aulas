a261c8e9c33e6c0d0afdd21e433e39d3e9324a54
J-Breno <joao.breno85@hotmail.com>
2024-11-19 08:23:43 -0300

    Cart clear

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -2,11 +2,12 @@ import "./styles.css";
 import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
-import * as productService from "../../../services/product-service";
 import { useNavigate, useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { ProductDTO } from "../../../models/product";
+import * as productService from "../../../services/product-service";
+import * as cartService from "../../../services/cart-service";
 
 export default function ProductDetails() {
   const params = useParams();
@@ -15,25 +16,30 @@ export default function ProductDetails() {
 
   const [product, setProduct] = useState<ProductDTO>();
   useEffect(() => {
+    productService
+      .findById(Number(params.productId))
+      .then((response) => {
+        console.log(response.data);
+        setProduct(response.data);
+      })
+      .catch(() => {
+        navigate("/");
+      });
+  }, []);
 
-    productService.findById(Number(params.productId))
-    .then(response => {
-      console.log(response.data);
-      setProduct(response.data)
-    })
-    .catch(() => {
-      navigate("/")
-    });
-  }, [])
+  function handleBuyClcik() {
+    if (product) cartService.addProduct(product);
+    navigate("/cart")
+  }
 
   return (
     <main>
       <section id="product-details-section" className="dsc-container">
-        {product 
-        && <ProductDetailsCard product={product} />
-        }
+        {product && <ProductDetailsCard product={product} />}
         <div className="dsc-btn-page-container">
-          <ButtonPrimary text="Comprar" />
+          <div onClick={handleBuyClcik}>
+            <ButtonPrimary text="Comprar" />
+          </div>
           <Link to={"/"}>
             <ButtonInverse text="Início" />
           </Link>
